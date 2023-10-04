f46a1fd0890a1da5d44d0856bae1bffcfe46d514
Srushtichopade <140955471+Srushtichopade@users.noreply.github.com>
2023-10-04 08:36:49 -0500

    Add files via upload

---
+++ List.py
@@ -0,0 +1,29 @@
+list=[3, 5,4, -5, -3, 10, 20, 100]
+l=[10,20,40,60,100]
+
+
+print(list[0])   
+
+print(list.index(4))
+
+largest = max(list)
+print("Largest number:", largest)
+
+smallest = min(list)
+print("Smallest number :",smallest)
+
+Sum = sum(list)
+print("Sum of list ",Sum)
+
+LEnght = len(list)
+print("Lenght ",LEnght)
+
+Sort = sorted(list)
+print("sorted the list ",Sort)
+
+list.extend([200,300])
+print(list)
+
+
+
+
